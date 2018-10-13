4b037864533524d80fdbfcb48587b829cd8efbde
Anne Jizrelle Jose <annejizrelle@gmail.com>
2018-10-13 23:44:54 +0800

    Intro to Component

--- multiple-instances/app.js
+++ multiple-instances/app.js
@@ -42,3 +42,44 @@ const two = new Vue({
 
 // This will run automatically upon refresh.
 // two.title = 'Changed Title from outside the two Vue Instances';
+
+
+/*****************************
+ *  COMPONENTS
+ * **************************/
+
+Vue.component('information', {
+    template: `
+        <div>
+            <p>This is an example of a component. It is reusable to both vue instances.</p>
+            <p>I like the color {{ color }}.</p>
+            <button v-on:click="changeColor">Change the color.</button>
+        </div>
+    `,
+    
+    data: function() {
+        return {
+            color: 'Magenta',
+        }
+    },
+
+    methods: {
+        changeColor: function() {
+            this.color = 'Periwinkle';
+        }
+    }
+});
+
+new Vue({
+    el: '#vue-app-first',
+    data: {
+        title: 'First Vue App',
+    }
+});
+
+new Vue({
+    el: '#vue-app-second',
+    data: {
+        title: 'Second Vue App',
+    }
+});
